3f42ccb4cc51acab5ba3f090f08ba3833be25976
Dawitlior <dawitlior777@gmail.com>
2022-11-29 21:14:17 +0200

    add

---
+++ server/config/passport.js
@@ -0,0 +1,24 @@
+const keys = require("./keys");
+const Auth = require("../models/users-model");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+
+module.exports = (passport) => {
+  const options = {
+    secretOrKey: keys.secretKey,
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  };
+  passport.use(
+    new JwtStrategy(options, (jwt_payload, done) => {
+      Auth.findById(jwt_payload.id)
+        .then((user) => {
+          console.log("users found");
+          return done(null, false);
+        })
+        .catch((err) => {
+          console.log(err.message);
+          return done(err);
+        });
+    })
+  );
+}; 
